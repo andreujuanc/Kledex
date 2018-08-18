e100ea6bdf0e3bc8b4d0de02d9107d277d1c6ed0
Juan Carlos Andreu Gutiérrez <andreujuanc@gmail.com>
2018-08-18 20:58:03 +0200

    Adding services, for some reason didnt get them :S

---
+++ src/OpenCqrs.Store.InMemory/InMemoryBusMessageDispatcher.cs
@@ -0,0 +1,14 @@
+﻿using OpenCqrs.Bus;
+using System;
+using System.Threading.Tasks;
+
+namespace OpenCqrs.Store.InMemory
+{
+    public class InMemoryBusMessageDispatcher : IBusMessageDispatcher
+    {
+        public Task DispatchAsync<TMessage>(TMessage message) where TMessage : IBusMessage
+        {
+            throw new NotImplementedException();
+        }
+    }
+}    
--- src/OpenCqrs.Store.InMemory/ServiceCollectionExtensions.cs
+++ src/OpenCqrs.Store.InMemory/ServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OpenCqrs.Bus;
+using OpenCqrs.Dependencies;
+using OpenCqrs.Domain;
 using OpenCqrs.Extensions;
 using System;
 
@@ -14,6 +17,15 @@ namespace OpenCqrs.Store.InMemory
 
             if (configuration == null)
                 throw new ArgumentNullException(nameof(configuration));
+
+            var store = new InMemoryStore();
+            var busdistpacher = new InMemoryBusMessageDispatcher();
+
+            builder.Services.AddSingleton<IEventStore>((sp) => store);
+            builder.Services.AddSingleton<ICommandStore>((sp) => store);
+            builder.Services.AddSingleton<IBusMessageDispatcher>((sp) => busdistpacher);
+            builder.Services.AddSingleton<IResolver, Resolver>();
+            
 
             builder.Services.Scan(s => s
                 .FromAssembliesOf(typeof(InMemoryStore))
